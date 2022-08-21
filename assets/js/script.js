b0a2da5ad02eca8b723d9fde09729465f4b746f0
Christopher C Ray <110488249+cray412@users.noreply.github.com>
2022-08-21 07:04:13 -0400

    quiz section working, adding randomization to questions

--- assets/js/script.js
+++ assets/js/script.js
@@ -50,23 +50,71 @@ var question5 = {
     correctAnswer: "option4"
 }
 
+var question6 = {
+    question: "6. Question #6",
+    option1: "a",
+    option2: "b",
+    option3: "c",
+    option4: "d",
+    correctAnswer: "option1"
+}
+
+var question7 = {
+    question: "7. Question #7",
+    option1: "a",
+    option2: "b",
+    option3: "c",
+    option4: "d",
+    correctAnswer: "option1"
+}
+
+var question8 = {
+    question: "8. Question #8",
+    option1: "a",
+    option2: "b",
+    option3: "c",
+    option4: "d",
+    correctAnswer: "option2"
+}
+
+var question9 = {
+    question: "9. Question #9",
+    option1: "a",
+    option2: "b",
+    option3: "c",
+    option4: "d",
+    correctAnswer: "option2"
+}
+
+var question10 = {
+    question: "10. Question #10",
+    option1: "a",
+    option2: "b",
+    option3: "c",
+    option4: "d",
+    correctAnswer: "option4"
+}
 var i = 0;
-var questionBank = [question1, question2, question3, question4, question5]
+var questionBank = [question1, question2, question3, question4, question5, question6, question7, question8, question9, question10];
 var currentQuestionObject = "question"
 var correctAnswer = "";
 var chosenAnswer = "";
 
 function init() {
 
-    function askQuestion (){
-    result.textContent = "";
-    currentQuestion.textContent = questionBank[i].question;
-    option1.textContent = questionBank[i].option1;
-    option2.textContent = questionBank[i].option2;
-    option3.textContent = questionBank[i].option3;
-    option4.textContent = questionBank[i].option4;
-    correctAnswer = questionBank[i].correctAnswer;
-    console.log(correctAnswer);
+    function askQuestion() {
+        result.textContent = "";
+        currentQuestion.textContent = questionBank[i].question;
+        option1.textContent = questionBank[i].option1;
+        option2.textContent = questionBank[i].option2;
+        option3.textContent = questionBank[i].option3;
+        option4.textContent = questionBank[i].option4;
+        correctAnswer = questionBank[i].correctAnswer;
+        document.querySelector("#answer1").disabled = false;
+        document.querySelector("#answer2").disabled = false;
+        document.querySelector("#answer3").disabled = false;
+        document.querySelector("#answer4").disabled = false;
+        console.log(correctAnswer);
     }
 
     function getAnswer() {
@@ -76,55 +124,82 @@ function init() {
         }
         else {
             result.textContent = "INCORRECT!";
-
         }
     }
 
+    function disableButtons(){
+        
+        document.querySelector("#answer1").disabled = true;
+        document.querySelector("#answer2").disabled = true;
+        document.querySelector("#answer3").disabled = true;
+        document.querySelector("#answer4").disabled = true;
+    }
+
+    function quizFinished(){
+        window.location.href = "./finished.html"
+    }
+
     option1.addEventListener("click", function () {
+        disableButtons();
         console.log("You chose answer 'A'");
         chosenAnswer = "option1";
         getAnswer();
-        if (i < 10) {
-        i++;
-        }
+        if (i < questionBank.length - 1) {
+            i++;
         console.log(i);
         setTimeout(askQuestion, 1000);
+        }
+        else {
+            setTimeout(quizFinished, 1000);
+        }
     }
     )
 
     option2.addEventListener("click", function () {
+        disableButtons();
         console.log("You chose answer 'B'");
         chosenAnswer = "option2";
         getAnswer();
-        if (i < 10) {
-        i++;
-        }
+        if (i < questionBank.length - 1) {
+            i++;
         console.log(i);
         setTimeout(askQuestion, 1000);
+        }
+        else {
+            setTimeout(quizFinished, 1000);
+        }
     }
     )
 
     option3.addEventListener("click", function () {
+        disableButtons();
         console.log("You chose answer 'C'");
         chosenAnswer = "option3";
         getAnswer();
-        if (i < 10) {
-        i++;
-        }
+        if (i < questionBank.length - 1) {
+            i++;
         console.log(i);
         setTimeout(askQuestion, 1000);
+        }
+        else {
+            setTimeout(quizFinished, 1000);
+        }
     }
     )
 
     option4.addEventListener("click", function () {
+        disableButtons();
         console.log("You chose answer 'D'");
         chosenAnswer = "option4";
         getAnswer();
-        if (i < 10) {
-        i++;
-        }
+        if (i < questionBank.length - 1) {
+            i++;
         console.log(i);
         setTimeout(askQuestion, 1000);
+        }
+        else {
+            setTimeout(quizFinished, 1000);
+        }
     }
     )
     console.log(i)
