3b91ca7eafc685359cd41ce55fea2cb21a81683d
Christopher C Ray <110488249+cray412@users.noreply.github.com>
2022-08-24 02:25:37 -0400

    finished project

--- assets/js/script2.js
+++ assets/js/script2.js
@@ -1,32 +1,44 @@
 var scoreList = document.querySelector("#score-list");
-
 var initials = localStorage.getItem("initials");
 var score = localStorage.getItem("score");
 var scoreEntry = initials + " - " + score;
-var newScoreList = localStorage.getItem("highscoreList");
+
 var highscores = [];
 
-highscores.push(scoreEntry);
-
-if (highscores != null) {
-    highscores.push(newScoreList);
-}
-
-var highscoreList = localStorage.setItem("highscoreList", highscores);
 
 function postScores() {
-    // scoreList.innerHTML = "";
-
+    scoreList.innerHTML = "";
 
     for (var i = 0; i < highscores.length; i++) {
-        // var scores = highscores[i];
+        var scores = highscores[i];
 
         var li = document.createElement("li");
-        li.textContent = highscores[i];
+        li.textContent = scores;
         li.setAttribute("data-index", i);
         scoreList.appendChild(li);
 
     }
 }
 
-postScores()
+function init() {
+
+    var savedScores = JSON.parse(localStorage.getItem("highscores"));
+
+    if (savedScores !== null) {
+        highscores = savedScores;
+    }
+
+    postScores();
+}
+
+function saveScores() {
+    localStorage.setItem("highscores", JSON.stringify(highscores));
+}
+
+highscores.push(scoreEntry);
+init();
+
+saveScores();
+postScores();
+
+
